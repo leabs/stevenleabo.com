542587d6a573f0cf43c19f8213a795845b79144a
Steve <stevenleabo@gmail.com>
2023-01-25 19:45:38 -0500

    adding back

--- assets/js/home-header.js
+++ assets/js/home-header.js
@@ -1,16 +1,16 @@
 function talk(hey) {
-  var audio = new Audio("../assets/" + hey + ".m4a");
+  var audio = new Audio("../assets/" + hey + ".mp3");
   audio.play();
 }
 function talk(whatsup) {
-  var audio = new Audio("../assets/" + whatsup + ".m4a");
+  var audio = new Audio("../assets/" + whatsup + ".mp3");
   audio.play();
 }
 function talk(itsme) {
-  var audio = new Audio("../assets/" + itsme + ".m4a");
+  var audio = new Audio("../assets/" + itsme + ".mp3");
   audio.play();
 }
 function talk(steve) {
-  var audio = new Audio("../assets/" + steve + ".m4a");
+  var audio = new Audio("../assets/" + steve + ".mp3");
   audio.play();
 }
